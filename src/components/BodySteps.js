7215e2c4cbd78c45a0fd2ba1b0539c54cb787306
Николаев Максим Геннадьевич <MGNikolaev@tsc.ts>
2024-07-03 13:59:45 +0300

    review code

--- src/components/BodySteps.js
+++ src/components/BodySteps.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function BodySteps({title, body}) {
-   const numberStep = title
-   const bodyStep = body
+function BodySteps({ title, body }) {
+  const numberStep = title
+  const bodyStep = body
   return (
     <div className='section container'>
       <section className="block">
-         <h2 className='block__title'>{numberStep}</h2>
-         <div className="block__body">{bodyStep}</div>
+        <h2 className='block__title'>{`Шаг ${numberStep}`}</h2>
+        <div className="block__body">{bodyStep}</div>
       </section>
     </div>
   )
